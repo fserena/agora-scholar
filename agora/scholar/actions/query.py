560d86aef9dcdab28cff70504f0f8b8cae815a77
Fernando Serena <fernando.serena@centeropenmiddleware.com>
2016-03-07 09:31:55 +0100

    Implemented algorithm for query building based on STOA.Root characterization and OPTIONALs

--- agora/scholar/actions/query.py
+++ agora/scholar/actions/query.py
@@ -33,6 +33,7 @@ from agora.stoa.actions.core import STOA
 from agora.stoa.actions.core.fragment import FragmentRequest, FragmentAction, FragmentSink
 from agora.stoa.actions.core.utils import chunks, tp_parts
 from shortuuid import uuid
+import networkx as nx
 
 __author__ = 'Fernando Serena'
 
@@ -84,14 +85,15 @@ class QuerySink(FragmentSink):
     """
     Query sink does not need any extra behaviour
     """
+
     def _remove(self, pipe):
         super(QuerySink, self)._remove(pipe)
 
     def __init__(self):
         super(QuerySink, self).__init__()
 
-    def _save(self, action):
-        super(QuerySink, self)._save(action)
+    def _save(self, action, general=True):
+        super(QuerySink, self)._save(action, general)
 
     def _load(self):
         super(QuerySink, self)._load()
@@ -122,7 +124,7 @@ class QueryResponse(FragmentConsumerResponse):
                                                                            self.sink.preferred_labels)))
 
             # Query result chunking, yields JSON
-            for ch in chunks(result, 100):
+            for ch in chunks(result, 10):
                 result_rows = []
                 for t in ch:
                     if any(t):
@@ -139,7 +141,7 @@ class QueryResponse(FragmentConsumerResponse):
             raise
         finally:
             self.__fragment_lock.release()
-            yield '', {'state': 'end'}
+            yield [], {'state': 'end', 'format': 'json'}
 
         # Just after sending the state:end message, the request delivery state switches to sent
         self.sink.delivery = 'sent'
@@ -149,6 +151,17 @@ class QueryResponse(FragmentConsumerResponse):
         Query the fragment using the original request graph pattern
         :return: The query result
         """
+
+        def __build_query_from(x, depth=0):
+            def build_pattern_query((u, v, data)):
+                return '\nOPTIONAL { %s %s %s %s }' % (u, data['predicate'], v, __build_query_from(v, depth + 1))
+
+            out_edges = list(gp_graph.out_edges_iter(x, data=True))
+            out_edges = reversed(sorted(out_edges, key=lambda x: gp_graph.out_degree))
+            if out_edges:
+                return ' '.join([build_pattern_query(x) for x in out_edges])
+            return ''
+
         def __transform(x):
             """
             Trick to avoid literal language tags problem, etc.
@@ -158,17 +171,28 @@ class QueryResponse(FragmentConsumerResponse):
                 return '?%s FILTER(str(?%s) = %s)' % (var, var, x)
             return x
 
-        def __make_optional(x):
-            if 'FILTER' not in x and not x.startswith('<'):
-                x = 'OPTIONAL { %s }' % x
-            return x
+        gp = filter(lambda x: ' a ' not in x and 'rdf:type' not in x, self.sink.fragment_gp)
+        gp_parts = [[__transform(self.sink.map(part, fmap=True)) for part in tp_parts(tp)] for tp in gp]
 
-        gp = [' '.join([__transform(self.sink.map(part, fmap=True)) for part in tp_parts(tp)]) for tp in
-              self.sink.fragment_gp]
-        gp = map(__make_optional, gp)
-        where_gp = ' . '.join(gp)
+        blocks = []
+        filter_block = []
+        gp_graph = nx.DiGraph()
+        for gp_part in gp_parts:
+            if 'FILTER' not in gp_part[2]:
+                gp_graph.add_edge(gp_part[0], gp_part[2], predicate=gp_part[1])
+            else:
+                filter_block.append(' '.join(gp_part))
 
-        query = """SELECT %s WHERE { %s }""" % (' '.join(self.sink.preferred_labels), where_gp)
+        roots = filter(lambda x: gp_graph.in_degree(x) == 0, gp_graph.nodes())
+
+        blocks += [' %s a stoa:Root \n OPTIONAL { %s }' % (root, __build_query_from(root)) for root in roots]
+        if filter_block:
+            blocks.append('{ %s }' % ' .\n '.join(filter_block))
+
+        where_gp = ' .\n'.join(blocks)
+        projection = ' '.join(self.sink.preferred_labels) if self.sink.preferred_labels else '*'
+        query = """SELECT DISTINCT %s WHERE { %s }""" % (projection, where_gp)
+        log.debug(query)
 
         result = []
         try:
